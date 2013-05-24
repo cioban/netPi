93616de4b37c6cb385cc37cf1fa2cdfabfa0a112
Sergio Cioban Filho <cioban@cianet.ind.br>
2013-05-24 17:08:33 -0300

    Implemented system menu and uptime information

--- bin/_menu.py
+++ bin/_menu.py
@@ -10,21 +10,10 @@ __date__ = '24/05/2013 11:25:20 AM'
 import os
 import sys
 import time
-import socket
-import fcntl
-import struct
 
 from _lcd import PCD8544
 from _keypad import keypad
-
-def getIpAddress(ifname):
-    try:
-	    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	    return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915,
-                struct.pack('256s', ifname[:15]))[20:24])
-    except Exception:
-        return ''
-
+from _system import get_ip, get_uptime
 
 class navigation_menu:
 
@@ -48,8 +37,6 @@ class navigation_menu:
         if self.menu_pos >= len(self.menu_screens):
             self.menu_pos = 0
         self.lcd.cls()
-        menu_function = eval('self.menu_'+self.menu_screens[self.menu_pos])
-        menu_function()
 
     def menu_pos_up(self):
         self.menu_pos = self.menu_pos + 1
@@ -67,7 +54,7 @@ class navigation_menu:
             self.lcd.centre_word(0,"netPi")
             self.lcd.centre_word(1,": ready :")
             self.lcd.centre_word(3,"Use as teclas")
-            ip = getIpAddress('eth0')
+            ip = get_ip('eth0')
             self.lcd.centre_word(5,ip)
 
         for key, value in self.pad.key_value.iteritems():
@@ -92,14 +79,14 @@ class navigation_menu:
     def menu_system(self):
         if self.menu_pos != self.last_menu_pos:
             self.last_menu_pos = self.menu_pos
-            self.lcd.gotoxy(0,1)
+            self.lcd.gotoxy(0,0)
             self.lcd.text(":netPi: system")
             self.lcd.gotoxy(0,2)
             self.lcd.text("CPU:")
             self.lcd.gotoxy(0,3)
             self.lcd.text("MEM:")
             self.lcd.gotoxy(0,4)
-            self.lcd.text("UPTIME:")
+            self.lcd.text("UP: %s" % get_uptime())
 
         for key, value in self.pad.key_value.iteritems():
             if key == 'six' and value is True:
@@ -113,7 +100,7 @@ class navigation_menu:
                 self.pad.read()
                 menu_function = eval('self.menu_'+self.menu_screens[self.menu_pos])
                 menu_function()
-                time.sleep(0.2)
+                time.sleep(0.05)
 
         except KeyboardInterrupt:
             self.kill_handler(0, '')
